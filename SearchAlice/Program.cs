8ad8f8a3618986f508ab16d677a035f8f2908c1b
Edilma Riano <edilmaria@hotmail.com>
2019-10-24 05:50:04 -0400

    Practice file following the video of learning c# with microsoft

--- SearchAlice/Program.cs
+++ SearchAlice/Program.cs
@@ -1,28 +1,43 @@
 ﻿using System;
 
-namespace SearchAlice
+namespace Alice
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string aliceString = "Alice was beginning to get very tired of sitting" +
-                " by her sister on the bank, and of having nothing to do: once or " +
-                "twice she had peeped into the book her sister was reading, but " +
-                "it had no pictures or conversations in it, 'and what is the use " +
-                "of a book,' thought Alice 'without pictures or conversation?";
-            
-            Console.WriteLine("What string you want to search in Alice in Wonderland? ");
+            string paragraph = "Alice was beginning to get very tired of sitting" +
+                " by her sister on the bank, and of having nothing to do: once or" +
+                " twice she had peeped into the book her sister was reading, " +
+                "but it had no pictures or conversations in it, 'and what is " +
+                "the use of a book,' thought Alice 'without pictures or conversation?'";
+            Console.WriteLine("What word(s) you want to search for?  ");
+            string searchValue = Console.ReadLine();
 
-            string searchTerm = Console.ReadLine();
+            bool foundIt = findTerm(paragraph, searchValue);
 
-        }
-        private static bool FindString(string word)
-        {
-            if (true)
+            if (foundIt == true)
             {
-
+                Console.WriteLine("Your {0}, is in the Alice paragraph", searchValue);
             }
+            else
+            {
+                Console.WriteLine("Sorry,your {0}, is NOT  in the Alice paragraph", searchValue);
+            }
+
+
+
+
+
+
         }
+        private static bool findTerm(string paragraph, string term)
+        {
+            bool answer = paragraph.Contains(term);
+            return answer;
+        }
+
+
+
     }
 }
