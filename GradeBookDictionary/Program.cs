fd3b846ca5534e1a0827b72b3c7f946edcef184b
Edilma Riano <edilmaria@hotmail.com>
2019-10-26 19:29:56 -0400

    Started working exercises chapter 2

---
+++ GradeBookDictionary/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBookDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int studentId;
+            string studentName;
+
+            Console.WriteLine("To enter a new student information press enter or choose x to exit");
+
+
+            Dictionary<int, string> studentRoast = new Dictionary<int, string>();
+
+            
+            Console.WriteLine("What is your student number");
+
+            studentId = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("What is your name?" );
+
+            studentName = Console.ReadLine();
+
+            studentRoast.Add(studentId, studentName);
+            
+
+            Console.WriteLine("YOur name is {0}, welcome " ,studentRoast[studentId]);
+            Console.ReadLine();
+
+    
+            
+        }
+        /*
+         private static Dictionary<int, string> ReadStudents(Dictionary<int, string> studentRoast)
+        {
+            Console.WriteLine("What is your student number");
+            studentId = int.Parse(Console.ReadLine());
+            Console.WriteLine("What is your name?" );
+            studentName = Console.ReadLine();
+            studentRoast.Add(studentId, studentName);
+        }
+        */
+    }
+}
--- WorkingListVideo/Program.cs
+++ WorkingListVideo/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SimpleClasses
 {
     class Program
     {
         static void Main(string[] args)
-        {
+        { /*
             Car myCar = new Car();
             myCar.VIN = "15";
             myCar.Make = "Oldsmobile";
@@ -20,26 +21,26 @@ namespace SimpleClasses
                 myCar.Year,
                 myCar.Color);
             */
-
+            /*
             Car car1 = new Car();
-            car1.VIN = "16";
+            car1.VIN = "A16";
             car1.Make = "Honda";
             car1.Model = "Accord";
             car1.Year = 2014;
             car1.Color = "Bue";
 
             Car car2 = new Car();
-            car2.VIN = "17";
+            car2.VIN = "B17";
             car2.Make = "Lexus";
             car2.Model = "S3";
             car2.Year = 2016;
             car2.Color = "Black";
-
+            */
             //decimal value = DetermineMarketValue(myCar);
             //Console.WriteLine("{0:C} is the value of your car", value);
 
             //Console.WriteLine("The value of my car is {0:C} ", myCar.DetermineMakretValue());
-
+            /*
             List<Car> myList = new List<Car>();
             myList.Add(myCar);
             myList.Add(car1);
@@ -50,15 +51,37 @@ namespace SimpleClasses
                 Console.WriteLine(car.Model);
             }
 
-            Dictionary<string, Car>
+           
+            Dictionary<string, Car> myDictionary = new Dictionary<string, Car>();
+
+            myDictionary.Add(myCar.VIN, myCar);
+            myDictionary.Add(car1.VIN, car1);
+            myDictionary.Add(car2.VIN, car2);
+
+            Console.WriteLine(myDictionary["B17"].Make);
+            */
+
+            //string[] names = { "Bob", "Streve", "Brian", "Chuck" };
+
+            /*
+              Object initializer - No need of a constructor
+            Car car1 = new Car() { Make = "BMW", Model = "750li", VIN = "C13" };
+            Car car2 = new Car() { Make = "Toyota", Model = "4Runner", VIN = "D14" };
+            */
+
+            //Collection initializer
+
+            List<Car> myList = new List<Car>() {
+
+                new Car{Make = "Oldsmobile", Model = "Cutlas Supreme" , VIN="E5"};
+
+            new Car { Make = "Nissan", Model = "Altima", VIN = "F16" } };
+        }
 
 
 
-
-
-
-
-            Console.ReadLine();
+        Console.ReadLine();
+            }
         }
 
         private static decimal DetermineMarketValue(Car car)
