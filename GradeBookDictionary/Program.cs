0a2ee95b6d72c09dbf23d7b69f5a0f6c0dfde004
Edilma Riano <edilmaria@hotmail.com>
2019-10-27 06:49:23 -0400

    Worked with dictionary - Prep chapter 2

--- GradeBookDictionary/Program.cs
+++ GradeBookDictionary/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 
 namespace GradeBookDictionary
 {
@@ -7,41 +8,72 @@ namespace GradeBookDictionary
     {
         static void Main(string[] args)
         {
+            /*
             int studentId;
             string studentName;
 
             Console.WriteLine("To enter a new student information press enter or choose x to exit");
-
-
             Dictionary<int, string> studentRoast = new Dictionary<int, string>();
-
-            
             Console.WriteLine("What is your student number");
-
             studentId = int.Parse(Console.ReadLine());
-
             Console.WriteLine("What is your name?" );
-
             studentName = Console.ReadLine();
-
             studentRoast.Add(studentId, studentName);
-            
-
             Console.WriteLine("YOur name is {0}, welcome " ,studentRoast[studentId]);
             Console.ReadLine();
+            */
+            Dictionary<int, string> studentRoast = new Dictionary<int, string>();
 
-    
-            
+            //Console.WriteLine("To enter a new student name or press enter to exit");
+            //string wantToContinue = Console.ReadLine();
+            string wantToContinue;
+
+            do
+            {
+                askForStudents(studentRoast);
+                Console.WriteLine("Do you have more students?  Y/N");
+                wantToContinue = Console.ReadLine();
+
+            } while (wantToContinue !="N" );
+
+            printStudentRoast(studentRoast);
+
+
         }
-        /*
-         private static Dictionary<int, string> ReadStudents(Dictionary<int, string> studentRoast)
+        
+         private static void askForStudents(Dictionary<int, string> studentRoast)
         {
+            //int studentId;
+            //string studentName;
             Console.WriteLine("What is your student number");
-            studentId = int.Parse(Console.ReadLine());
+            int studentId = int.Parse(Console.ReadLine());
             Console.WriteLine("What is your name?" );
-            studentName = Console.ReadLine();
+            string studentName = Console.ReadLine();
             studentRoast.Add(studentId, studentName);
+            //return studentRoast;
         }
-        */
+
+        private static void printStudentRoast(Dictionary<int, string> studentRoaster)
+        {
+            //int studentId;
+            //string studentName;
+            StringBuilder myRoster = new StringBuilder();
+            /*
+            foreach (KeyValuePair<int, string> student in studentRoaster)
+            {
+                
+                Console.WriteLine(student.Key + "\t"
+                    + student.Value);
+            }
+            */
+            foreach (KeyValuePair<int, string> student in studentRoaster)
+            {
+                myRoster.Append(student.Key);
+                myRoster.Append("\t");
+                myRoster.Append(student.Value);
+                myRoster.Append("\n");
+            }
+            Console.WriteLine(myRoster);
+        }
     }
 }
